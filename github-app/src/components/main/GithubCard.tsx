d183bd9c3b7c699a61d95fc355884311a4f7a831
verzion2k <kovacs.zsoltzx@gmail.com>
2019-05-20 19:10:06 +0200

    First changes

---
+++ github-app/src/components/main/GithubCard.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class GithubCard extends Component {
+	render() {
+		return (
+			<main className="main">
+				<Input />
+				<Button />
+				<Content />
+			</main>
+		);
+	}
+}
